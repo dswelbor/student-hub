84e4ed41a94d03e22d22a890e3cd5e22fbfc3c9f
David Seth Welborn <dswelbor@asu.edu>
2019-10-15 02:40:27 -0400

    flashcard models.py in progress

---
+++ studenthub/academics/flashcard/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+
+
+class Subject(models.Model):
+    """
+    Simple Schema to outline established Subjects for flashcards
+    """
+    # Enumerated Subject
+    subject = models.CharField(max_length=64, unique=True)
+    # Subject description
+    description = models.CharField(max_length=128, blank=True, null=True)
+
+    def __str__(self):
+        return self.subject
+
+
+class Course(models.Model):
+    """
+    Simple Schema to outline established Courses for flashcards
+    """
+    # Enumerated Course
+    course = models.CharField(max_length=64, unique=True)
+    # Referential Subject
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    # Course description
+    description = models.CharField(max_length=128, blank=True, null=True)
+
+    def __str__(self):
+        return self.course
+
+class Module(models.Model):
+    """
+    Simple Schema to model established Course Modules for flashcards.
+    """
+    # TODO: handle module, course composite key unicity in validation
+    # Enumerated Course Module
+    # Note: not unique as composite keys are not supported by current Django implementation
+    module = models.CharField(max_length=64)
+    # Referential Course
+    # TODO: revisit multiplicity
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    # Course description
+    description = models.CharField(max_length=128, blank=True, null=True)
+
+    def __str__(self):
+        return self.module
+
+
+class Flashcard(models.Model):
+    """
+    Simple Schema to model Flashcards
+    """
+    # Enumerated Course
+    question = models.CharField(max_length=1024, unique=True)
+    # Flashcard short answer
+    answer = models.CharField(max_length=64)
+    # Referential Module - gets Course Candidate key from module attributes
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    # Approval - is approved?
+    approved = models.BooleanField(default=True)
+
+    # Course description
+    description = models.CharField(max_length=128, blank=True, null=True)
+
+    # TODO: Finish implementing these fields
+
+    def __str__(self):
+        return self.question
