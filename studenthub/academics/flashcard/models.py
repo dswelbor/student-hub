fc4a34532c9a50233ca104ae5231d103411c777d
David Seth Welborn <dswelbor@asu.edu>
2019-10-15 22:56:05 -0400

    removed duplicate model class - fixed styling

--- studenthub/academics/flashcard/models.py
+++ studenthub/academics/flashcard/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from django.db import models
 from config.settings import AUTH_USER_MODEL
 
@@ -31,14 +33,16 @@ class Course(models.Model):
     def __str__(self):
         return self.course
 
+
 class Module(models.Model):
     """
     Simple Schema to model established Course Modules for flashcards. Any one Module can only reference
-    one Course, but a Course can be referenced by many Modules.
+    one Course, but a Course can be referenced by many Modules. A module has the composite primary
+    key (CourseName, ModuleName)
     """
     # TODO: handle module, course composite key unicity in validation
     # Enumerated Course Module
-    # Note: not unique as composite keys are not supported by current Django implementation
+    # Note: unique=True unused as composite keys are not supported by current Django implementation
     module = models.CharField(max_length=64)
     # Referential Course
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -81,22 +85,6 @@ class FlashcardTag(models.Model):
 
 class FlashcardStats(models.Model):
     """
-    Simple Schema to model Flashcards statistics
-    """
-    # User - relates flashcard data to specific user
-    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
-    # Flashcard - cumulative stats for a given flashcard
-    flashcard = models.ForeignKey(Flashcard, on_delete=models.CASCADE)
-    # TODO: implement remained for fields
-
-
-    def __str__(self):
-        return self.flashcard.question
-
-
-
-class Flashcard(models.Model):
-    """
     Schema to model user-specific Flashcard-specific stats
     """
     # User using Flashcard
@@ -108,15 +96,9 @@ class Flashcard(models.Model):
     # Number of total attempts using flashcard
     attempted = models.PositiveSmallIntegerField(default=0, blank=False, null=False)
     # Cumulative time spent on flashcard
-    time = models.TimeField() # TODO: set defaults
+    elapsed = models.DurationField(default=timedelta())
     # last attempt datetime
-    lastAttempt = models.DateTimeField() # TODO: set defaults
+    last_attempt = models.DateTimeField(default=datetime.now())
 
-    # Enumerated Course
-    question = models.CharField(max_length=1024, unique=True)
-    # Flashcard short answer
-    answer = models.CharField(max_length=64)
-    # Referential Module - gets Course Candidate key from module attributes
-    module = models.ForeignKey(Module, on_delete=models.CASCADE)
-    # Approval - is approved?
-    approved = models.BooleanField(default=False)
+    def __str__(self):
+        return self.flashcard
