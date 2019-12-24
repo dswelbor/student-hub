09e926f1e721a855699c1b9d1bd97db20c3fa97e
David Seth Welborn <dswelbor@asu.edu>
2019-12-23 23:08:42 -0500

    Clean up comments

--- studenthub/academics/flashcard/manage.py
+++ studenthub/academics/flashcard/manage.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-from django.db.models import Q, F, FloatField, ExpressionWrapper
-# from .models import Subject, Course, Module
-
 
 class FlashcardManager(models.Manager):
+    """Custom model manager for custom Flashcard querysets"""
     def get_flashcards(self, qty, subject=None, course=None, module=None):
         """Returns a QuerySet of random questions that meet passed criteria"""
 
@@ -12,23 +10,13 @@ class FlashcardManager(models.Manager):
         flashcards_rng = super(FlashcardManager, self).get_queryset().order_by('?')
 
         if subject:
-            # subjects = Subject.objects.get_queryset().filter(subject=subject)
             flashcards_rng = flashcards_rng.filter(module__course__subject__subject=subject)
-        # else:
-            # subjects = Subject.objects.get_queryset()
 
         if course:
             flashcards_rng = flashcards_rng.filter(module__course__course=course)
-            # courses = Subject.objects.get_queryset().filter(course=course)
-        # else:
-            # courses = Subject.objects.get_queryset().filter(course__in=subjects)
 
         if module:
             flashcards_rng = flashcards_rng.filter(module__module=module)
-            # flashcards_rng.filter(module__module=module)
-        # else:
-            #flashcards_rng.filter(module__in=courses)
-
 
         # Return random flashcards
         return flashcards_rng[:qty]
