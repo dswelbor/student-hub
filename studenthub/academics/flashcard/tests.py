6af218d768292246a45206ed8ea47533ff534476
dswelbor <dswelbor@asu.edu>
2020-01-02 21:59:26 -0500

    Add get_flashcards unit test

--- studenthub/academics/flashcard/tests.py
+++ studenthub/academics/flashcard/tests.py
@@ -3,10 +3,14 @@ from academics.flashcard.models import Course, Flashcard, Module, Subject
 
 
 class FlashcardTestCase(TestCase):
+    MATH_SUBJECT = 'MATH-TEST'
+    ENG_SUBJECT = 'ENGINEERING-TEST'
+
     def setUp(self):
+        """Setup test db with test flashcard records"""
         # Init subjects, courses, and modules for tests
-        math = Subject.objects.create(subject="MATH-TEST")
-        eng = Subject.objects.create(subject="ENGINEERING-TEST")
+        math = Subject.objects.create(subject=self.MATH_SUBJECT)
+        eng = Subject.objects.create(subject=self.ENG_SUBJECT)
         fse000 = Course.objects.create(course="cat", subject=eng)
         mat266 = Course.objects.create(course="MAT266", subject=math)
         module0_fse000 = Module.objects.create(module="fse000-mod0", course=fse000)
@@ -61,20 +65,49 @@ class FlashcardTestCase(TestCase):
                                  answer="test789",
                                  module=module0_mat266)
 
-        # 3 flashcards in module1_mat266
+        # 2 flashcards in module1_mat266
         Flashcard.objects.create(question="This is a math test q 1abc",
                                  answer="test1abc",
                                  module=module1_mat266)
         Flashcard.objects.create(question="This is a math test q 1def",
                                  answer="test1def",
                                  module=module1_mat266)
-        Flashcard.objects.create(question="This is a math test q 1123",
-                                 answer="test1123",
-                                 module=module1_mat266)
 
     def test_new_flashcards_unapproved(self):
         """Flashcards are not approved by default"""
-        testabc = Flashcard.objects.get(id=1)
-        testdef = Flashcard.objects.get(id=2)
+        testabc = Flashcard.objects.all()[0]
+        testdef = Flashcard.objects.all()[1]
         self.assertEqual(testabc.approved, False)
         self.assertEqual(testdef.approved, False)
+
+    def test_custom_get_flashcards(self):
+        math = Subject.objects.get(subject=self.MATH_SUBJECT)
+        eng = Subject.objects.get(subject=self.ENG_SUBJECT)
+        math_flashcards = Flashcard.custom.get_flashcards(10, subject=math)
+        math_flashcards_five = Flashcard.custom.get_flashcards(5, subject=math)
+        eng_flashcards = Flashcard.custom.get_flashcards(10, subject=eng)
+        mat266_mod1 = Module.objects.get(module='mat266-mod1')
+        mat266_mod1_flashcards = Flashcard.custom.get_flashcards(5, module=mat266_mod1)
+
+        # expect 7 math flashcards
+        self.assertEqual(math_flashcards.count(), 7)
+        # expect each card to be a math card
+        for card in math_flashcards:
+            self.assertEqual(card.module.course.subject.subject, self.MATH_SUBJECT)
+        # expect only 5 flashcards
+        self.assertEqual(math_flashcards_five.count(), 5)
+
+        # expect 8 engineering flashcards
+        self.assertEqual(eng_flashcards.count(), 8)
+        # expect each card to be an engineering card
+        for card in eng_flashcards:
+            self.assertEqual(card.module.course.subject.subject, self.ENG_SUBJECT)
+
+        # expect 2 math->mat266->mod1 cards
+        self.assertEqual(mat266_mod1_flashcards.count(), 2)
+        # expect each card to be a matt266-mod1 card
+        for card in mat266_mod1_flashcards:
+            self.assertEqual(card.module.module, 'mat266-mod1')
+
+
+
